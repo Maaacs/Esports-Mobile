5a082e68ade5135578131ce70fa4383d10006981
Maaacs <msb@icomp.ufam.edu.br>
2022-09-25 14:49:44 -0400

    Logo

--- mobile/src/Screens/Home/index.tsx
+++ mobile/src/Screens/Home/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image
+      source={logoImg}
+      style={styles.logo}
+      />
 
     </View>
   );
