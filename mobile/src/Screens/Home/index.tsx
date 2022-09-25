fa490714a61834b17ebee96ef991bfbaf8227fde
Maaacs <msb@icomp.ufam.edu.br>
2022-09-25 15:35:36 -0400

    inserindo primeiro jogo

--- mobile/src/Screens/Home/index.tsx
+++ mobile/src/Screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+
 import { Image, View } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+
 import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
@@ -17,6 +21,10 @@ export function Home() {
       <Heading 
       title="Encontre seu duo!"
       subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard 
+      data={GAMES[0]}
       />
 
     </View>
